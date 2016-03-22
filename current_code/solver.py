ec4e20954c2bb33921aa8525c7c6039660bd73b8
JSKenyon <jskenyon007@gmail.com>
2016-03-22 16:19:04 +0200

    Code for computing the JHR term is working.

--- current_code/solver.py
+++ current_code/solver.py
@@ -1,7 +1,6 @@
 from msread import *
 from time import time, sleep
 
-
 ms = DataHandler("WESTERBORK_POINT.MS")
 ms.fetch_all()
 ms.define_chunk(1,1)
@@ -15,7 +14,8 @@ def compute_jhr(obser_arr, model_arr, gains):
     spec_eye = np.zeros([2,4])
     spec_eye[(0,1),(0,3)] = 1
 
-    # MCG = np.einsum("...ij,...jk->...ik", model_arr[...,:,0,:,:], gains)
+    blah = list(model_arr.shape)
+    blah[-3:] = [4,1]
 
     GI = np.einsum("...ij,...jk->...ik", gains, spec_eye)
 
@@ -25,18 +25,29 @@ def compute_jhr(obser_arr, model_arr, gains):
 
     RGMH = np.einsum("...ij,...jk->...ik", RG, MH)
 
-    blah = list(RGMH.shape)
-    blah[-2:] = [4,1]
+    GIRGMH = np.einsum("...ij,...jk->...ik", GI, RGMH.reshape(blah))
 
-    GIRGMH = np.einsum("...ij,...jk->...i", GI, RGMH.reshape(blah))
+    JHR = -2 * np.sum(GIRGMH, axis=-3).imag
 
-    JHR = -2 * np.sum(GIRGMH, axis=-2).imag
+    print JHR
 
     return JHR
 
-    # print RGMH[:,:,:,(0,1),(0,1)]
-
-    # print gains[1,:,:].dot(spec_eye).dot(np.kron(tst[1,:,:], np.eye(2)))
-
 compute_jhr(a, b, "a")
 # print b[0,0,1,0,:].reshape(2,2).dot(np.eye(2))
+
+# CODE FOR COMPUTING JHR WITH EXPLICIT, INCONVENIENT KRON PROD.
+
+# MCG = np.einsum("...ij,...jk->...ik", model_arr[...,:,0,:,:], gains)
+#
+# res = np.empty([14,4,4], dtype=np.complex128)
+#
+# for i in xrange(MCG.shape[-3]):
+#     res[i,:,:] = np.kron(MCG[0,0,i,:,:], np.eye(2))
+#
+# res = np.einsum("...ij,...jk->...ik", GI, res)
+#
+# res = np.einsum("...ij,...jk->...ik", res, obser_arr[...,0,:,:,:].reshape(
+#     blah))
+#
+# print -2 * np.sum(res, axis=-3).imag
