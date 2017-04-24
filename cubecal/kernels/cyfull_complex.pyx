e94eeee2c13c7fe4a873c9b3ab9344291d466b89
JSKenyon <jskenyon007@gmail.com>
2017-04-24 14:34:23 +0200

    Some work towards models dependent kernels.

--- cubecal/kernels/cyfull_complex.pyx
+++ cubecal/kernels/cyfull_complex.pyx
@@ -11,194 +11,196 @@ ctypedef fused complex3264:
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_residual(complex3264 [:,:,:,:,:,:,:] m,
+def cycompute_residual(np.ndarray[complex3264, ndim=8] m,
                        complex3264 [:,:,:,:,:,:] g,
                        complex3264 [:,:,:,:,:,:] gh,
-                       complex3264 [:,:,:,:,:,:] o,
-                       complex3264 [:,:,:,:,:,:] r,
+                       complex3264 [:,:,:,:,:,:,:] o,
+                       complex3264 [:,:,:,:,:,:,:] r,
                        int t_int,
                        int f_int):
 
     """
-    This reduces the dimension of in1 to match out1. This is achieved by a
-    summation of blocks of dimension (t_int, f_int).
+    This computes the residual, resulting in large matrix indexed by 
+    (direction, model, time, frequency, antenna, antenna, correclation, correlation).
     """
 
-    cdef int d, t, f, aa, ab, rr, rc = 0
-    cdef int n_dir, n_tim, n_fre, n_ant
+    cdef int d, i, t, f, aa, ab, rr, rc = 0
+    cdef int n_dir, n_mod, n_tim, n_fre, n_ant
 
     n_dir = m.shape[0]
-    n_tim = m.shape[1]
-    n_fre = m.shape[2]
-    n_ant = m.shape[3]
+    n_mod = m.shape[1]
+    n_tim = m.shape[2]
+    n_fre = m.shape[3]
+    n_ant = m.shape[4]
 
     for d in xrange(n_dir):
-        for t in xrange(n_tim):
-            rr = t/t_int
-            for f in xrange(n_fre):
-                rc = f/f_int
-                for aa in xrange(n_ant):
-                    for ab in xrange(n_ant):
-                        r[t,f,aa,ab,0,0] = o[t,f,aa,ab,0,0] - (
-                        g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
-                        g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
-                        g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
-                        g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                rr = t/t_int
+                for f in xrange(n_fre):
+                    rc = f/f_int
+                    for aa in xrange(n_ant):
+                        for ab in xrange(n_ant):
+                            r[i,t,f,aa,ab,0,0] = o[i,t,f,aa,ab,0,0] - (
+                            g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
+                            g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
+                            g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
+                            g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
 
-                        r[t,f,aa,ab,0,1] = o[t,f,aa,ab,0,1] - (
-                        g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
-                        g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
-                        g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
-                        g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
+                            r[i,t,f,aa,ab,0,1] = o[i,t,f,aa,ab,0,1] - (
+                            g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
+                            g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
+                            g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
+                            g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
 
-                        r[t,f,aa,ab,1,0] = o[t,f,aa,ab,1,0] - (
-                        g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
-                        g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
-                        g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
-                        g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
+                            r[i,t,f,aa,ab,1,0] = o[i,t,f,aa,ab,1,0] - (
+                            g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
+                            g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
+                            g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
+                            g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
 
-                        r[t,f,aa,ab,1,1] = o[t,f,aa,ab,1,1] - (
-                        g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
-                        g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
-                        g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
-                        g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
+                            r[i,t,f,aa,ab,1,1] = o[i,t,f,aa,ab,1,1] - (
+                            g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
+                            g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
+                            g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
+                            g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jh(complex3264 [:,:,:,:,:,:,:] m,
+def cycompute_jh(np.ndarray[complex3264, ndim=8] m,
                  complex3264 [:,:,:,:,:,:] g,
-                 complex3264 [:,:,:,:,:,:,:] jh,
+                 np.ndarray[complex3264, ndim=8] jh,
                  int t_int,
                  int f_int):
 
     """
-    This reduces the dimension of in1 to match out1. This is achieved by a
-    summation of blocks of dimension (t_int, f_int).
+    This computes the the non-zero elements of jh. The result does have a model index.  
     """
 
-    cdef int d, t, f, aa, ab, rr, rc = 0
-    cdef int n_dir, n_tim, n_fre, n_ant
+    cdef int d, i, t, f, aa, ab, rr, rc = 0
+    cdef int n_dir, n_mod, n_tim, n_fre, n_ant
 
     n_dir = m.shape[0]
-    n_tim = m.shape[1]
-    n_fre = m.shape[2]
-    n_ant = m.shape[3]
+    n_mod = m.shape[1]
+    n_tim = m.shape[2]
+    n_fre = m.shape[3]
+    n_ant = m.shape[4]
 
     for d in xrange(n_dir):
-        for t in xrange(n_tim):
-            rr = t/t_int
-            for f in xrange(n_fre):
-                rc = f/f_int
-                for aa in xrange(n_ant):
-                    for ab in xrange(n_ant):
-                        jh[d,t,f,aa,ab,0,0] = \
-                                 g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,0] + \
-                                 g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,0]
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                rr = t/t_int
+                for f in xrange(n_fre):
+                    rc = f/f_int
+                    for aa in xrange(n_ant):
+                        for ab in xrange(n_ant):
+                            jh[d,i,t,f,aa,ab,0,0] = g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,0] + \
+                                                    g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,0]
 
-                        jh[d,t,f,aa,ab,0,1] = \
-                                 g[d,rr,rc,aa,0,0]*m[d,t,f,aa,ab,0,1] + \
-                                 g[d,rr,rc,aa,0,1]*m[d,t,f,aa,ab,1,1]
+                            jh[d,i,t,f,aa,ab,0,1] = g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,1] + \
+                                                    g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,1]
 
-                        jh[d,t,f,aa,ab,1,0] = \
-                                 g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,0] + \
-                                 g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,0]
+                            jh[d,i,t,f,aa,ab,1,0] = g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,0] + \
+                                                    g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,0]
 
-                        jh[d,t,f,aa,ab,1,1] = \
-                                 g[d,rr,rc,aa,1,0]*m[d,t,f,aa,ab,0,1] + \
-                                 g[d,rr,rc,aa,1,1]*m[d,t,f,aa,ab,1,1]
+                            jh[d,i,t,f,aa,ab,1,1] = g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1] + \
+                                                    g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jhr(complex3264 [:,:,:,:,:,:,:] jh,
-                  complex3264 [:,:,:,:,:,:] r,
+def cycompute_jhr(np.ndarray[complex3264, ndim=8] jh,
+                  complex3264 [:,:,:,:,:,:,:] r,
                   complex3264 [:,:,:,:,:,:] jhr,
                   int t_int,
                   int f_int):
 
     """
-    This reduces the dimension of in1 to match out1. This is achieved by a
-    summation of blocks of dimension (t_int, f_int).
+    This computes the jhr term on the GN/LM method. Note that while jh is indexed by model, the 
+    resulting jhr has no model index. 
     """
 
-    cdef int d, t, f, aa, ab, rr, rc = 0
-    cdef int n_dir, n_tim, n_fre, n_ant
+    cdef int d, i, t, f, aa, ab, rr, rc = 0
+    cdef int n_dir, n_mod, n_tim, n_fre, n_ant
 
     n_dir = jh.shape[0]
-    n_tim = jh.shape[1]
-    n_fre = jh.shape[2]
-    n_ant = jh.shape[3]
+    n_mod = jh.shape[1]
+    n_tim = jh.shape[2]
+    n_fre = jh.shape[3]
+    n_ant = jh.shape[4]
 
     for d in xrange(n_dir):
-        for t in xrange(n_tim):
-            rr = t/t_int
-            for f in xrange(n_fre):
-                rc = f/f_int
-                for aa in xrange(n_ant):
-                    for ab in xrange(n_ant):
-                        jhr[d,rr,rc,aa,0,0] = jhr[d,rr,rc,aa,0,0] + \
-                             r[t,f,aa,ab,0,0]*jh[d,t,f,ab,aa,0,0] + \
-                             r[t,f,aa,ab,0,1]*jh[d,t,f,ab,aa,1,0]
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                rr = t/t_int
+                for f in xrange(n_fre):
+                    rc = f/f_int
+                    for aa in xrange(n_ant):
+                        for ab in xrange(n_ant):
+                            jhr[d,rr,rc,aa,0,0] = jhr[d,rr,rc,aa,0,0] + \
+                                                    r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                                                    r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,0]
 
-                        jhr[d,rr,rc,aa,0,1] = jhr[d,rr,rc,aa,0,1] + \
-                             r[t,f,aa,ab,0,0]*jh[d,t,f,ab,aa,0,1] + \
-                             r[t,f,aa,ab,0,1]*jh[d,t,f,ab,aa,1,1]
+                            jhr[d,rr,rc,aa,0,1] = jhr[d,rr,rc,aa,0,1] + \
+                                                    r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                                                    r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,1]
 
-                        jhr[d,rr,rc,aa,1,0] = jhr[d,rr,rc,aa,1,0] + \
-                             r[t,f,aa,ab,1,0]*jh[d,t,f,ab,aa,0,0] + \
-                             r[t,f,aa,ab,1,1]*jh[d,t,f,ab,aa,1,0]
+                            jhr[d,rr,rc,aa,1,0] = jhr[d,rr,rc,aa,1,0] + \
+                                                    r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                                                    r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,0]
 
-                        jhr[d,rr,rc,aa,1,1] = jhr[d,rr,rc,aa,1,1] + \
-                             r[t,f,aa,ab,1,0]*jh[d,t,f,ab,aa,0,1] + \
-                             r[t,f,aa,ab,1,1]*jh[d,t,f,ab,aa,1,1]
+                            jhr[d,rr,rc,aa,1,1] = jhr[d,rr,rc,aa,1,1] + \
+                                                    r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                                                    r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,1]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jhj(complex3264 [:,:,:,:,:,:,:] jh,
+def cycompute_jhj(np.ndarray[complex3264, ndim=8] jh,
                   complex3264 [:,:,:,:,:,:] jhj,
                   int t_int,
                   int f_int):
 
     """
-    This reduces the dimension of in1 to match out1. This is achieved by a
-    summation of blocks of dimension (t_int, f_int).
+    This computes the approximation to the Hessian, jhj. 
     """
 
-    cdef int d, t, f, aa, ab, rr, rc = 0
-    cdef int n_dir, n_tim, n_fre, n_ant
+    cdef int d, i, t, f, aa, ab, rr, rc = 0
+    cdef int n_dir, n_mod, n_tim, n_fre, n_ant
 
     n_dir = jh.shape[0]
-    n_tim = jh.shape[1]
-    n_fre = jh.shape[2]
-    n_ant = jh.shape[3]
+    n_mod = jh.shape[1]
+    n_tim = jh.shape[2]
+    n_fre = jh.shape[3]
+    n_ant = jh.shape[4]
 
     for d in xrange(n_dir):
-        for t in xrange(n_tim):
-            rr = t/t_int
-            for f in xrange(n_fre):
-                rc = f/f_int
-                for aa in xrange(n_ant):
-                    for ab in xrange(n_ant):
-                        jhj[d,rr,rc,aa,0,0] = jhj[d,rr,rc,aa,0,0] + \
-                        jh[d,t,f,ab,aa,0,0].conjugate()*jh[d,t,f,ab,aa,0,0] + \
-                        jh[d,t,f,ab,aa,1,0].conjugate()*jh[d,t,f,ab,aa,1,0]
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                rr = t/t_int
+                for f in xrange(n_fre):
+                    rc = f/f_int
+                    for aa in xrange(n_ant):
+                        for ab in xrange(n_ant):
+                            jhj[d,rr,rc,aa,0,0] = jhj[d,rr,rc,aa,0,0] + \
+                            jh[d,i,t,f,ab,aa,0,0].conjugate()*jh[d,i,t,f,ab,aa,0,0] + \
+                            jh[d,i,t,f,ab,aa,1,0].conjugate()*jh[d,i,t,f,ab,aa,1,0]
 
-                        jhj[d,rr,rc,aa,0,1] = jhj[d,rr,rc,aa,0,1] + \
-                        jh[d,t,f,ab,aa,0,0].conjugate()*jh[d,t,f,ab,aa,0,1] + \
-                        jh[d,t,f,ab,aa,1,0].conjugate()*jh[d,t,f,ab,aa,1,1]
+                            jhj[d,rr,rc,aa,0,1] = jhj[d,rr,rc,aa,0,1] + \
+                            jh[d,i,t,f,ab,aa,0,0].conjugate()*jh[d,i,t,f,ab,aa,0,1] + \
+                            jh[d,i,t,f,ab,aa,1,0].conjugate()*jh[d,i,t,f,ab,aa,1,1]
 
-                        jhj[d,rr,rc,aa,1,0] = jhj[d,rr,rc,aa,1,0] + \
-                        jh[d,t,f,ab,aa,0,1].conjugate()*jh[d,t,f,ab,aa,0,0] + \
-                        jh[d,t,f,ab,aa,1,1].conjugate()*jh[d,t,f,ab,aa,1,0]
+                            jhj[d,rr,rc,aa,1,0] = jhj[d,rr,rc,aa,1,0] + \
+                            jh[d,i,t,f,ab,aa,0,1].conjugate()*jh[d,i,t,f,ab,aa,0,0] + \
+                            jh[d,i,t,f,ab,aa,1,1].conjugate()*jh[d,i,t,f,ab,aa,1,0]
 
-                        jhj[d,rr,rc,aa,1,1] = jhj[d,rr,rc,aa,1,1] + \
-                        jh[d,t,f,ab,aa,0,1].conjugate()*jh[d,t,f,ab,aa,0,1] + \
-                        jh[d,t,f,ab,aa,1,1].conjugate()*jh[d,t,f,ab,aa,1,1]
+                            jhj[d,rr,rc,aa,1,1] = jhj[d,rr,rc,aa,1,1] + \
+                            jh[d,i,t,f,ab,aa,0,1].conjugate()*jh[d,i,t,f,ab,aa,0,1] + \
+                            jh[d,i,t,f,ab,aa,1,1].conjugate()*jh[d,i,t,f,ab,aa,1,1]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
@@ -206,6 +208,11 @@ def cycompute_jhj(complex3264 [:,:,:,:,:,:,:] jh,
 @cython.nonecheck(False)
 def cycompute_jhjinv(complex3264 [:,:,:,:,:,:] jhj,
                      complex3264 [:,:,:,:,:,:] jhjinv):
+    """
+    This inverts the approximation to the Hessian, jhj. Note that asa as useful side effect, it is 
+    also suitable for inverting the gains.
+    """
+
 
     cdef int d, t, f, aa, ab = 0
     cdef int n_dir, n_tim, n_fre, n_ant
@@ -241,7 +248,8 @@ def cycompute_update(complex3264 [:,:,:,:,:,:] jhr,
                      complex3264 [:,:,:,:,:,:] jhj,
                      complex3264 [:,:,:,:,:,:] upd):
     """
-    NOTE: THIS RIGHT-MULTIPLIES THE ENTRIES OF IN1 BY THE ENTRIES OF IN2.
+    This computes the update by computing the product of jhj and jhr. These should already have been
+    reduced to the correct dimension so that this operation is very simple. 
     """
 
     cdef int d, t, f, aa = 0
@@ -282,8 +290,10 @@ def cycompute_corrected(complex3264 [:,:,:,:,:,:] o,
                         int f_int):
 
     """
-    This reduces the dimension of in1 to match out1. This is achieved by a
-    summation of blocks of dimension (t_int, f_int).
+    This computes the corrected visiblities, given the observed visiblities and the inverse of the 
+    gains. Note that the observed array expected here MUST NOT have a model index. This is because 
+    we only obtain a single gain estimate across all models and want to apply it to the raw the 
+    visibility data.
     """
 
     cdef int d, t, f, aa, ab, rr, rc = 0
