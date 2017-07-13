a3849301229eec414b1f4a9933187b3e3053b6ef
Ulrich Armel Mbou Sob <ulrich@elwood.ru.ac.za>
2017-07-13 12:55:54 +0200

    moved weights computation to python machines

--- cubical/kernels/cyfull_W_complex.pyx
+++ cubical/kernels/cyfull_W_complex.pyx
@@ -13,17 +13,16 @@ ctypedef fused complex3264:
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_residual(np.ndarray[complex3264, ndim=8] m,
-                       np.ndarray[complex3264, ndim=6] g,
-                       np.ndarray[complex3264, ndim=6] gh,
-                       np.ndarray[complex3264, ndim=7] o,
-                       np.ndarray[complex3264, ndim=7] r,
+def cycompute_residual(complex3264 [:,:,:,:,:,:,:,:] m,
+                       complex3264 [:,:,:,:,:,:] g,
+                       complex3264 [:,:,:,:,:,:] gh,
+                       complex3264 [:,:,:,:,:,:,:] r,
                        int t_int,
                        int f_int):
 
     """
     This computes the residual, resulting in large matrix indexed by 
-    (direction, model, time, frequency, antenna, antenna, correlation, correlation).
+    (direction, model, time, frequency, antenna, antenna, correclation, correlation).
     """
 
     cdef int d, i, t, f, aa, ab, rr, rc = 0
@@ -43,37 +42,38 @@ def cycompute_residual(np.ndarray[complex3264, ndim=8] m,
                     rc = f/f_int
                     for aa in xrange(n_ant):
                         for ab in xrange(n_ant):
-                            r[i,t,f,aa,ab,0,0] = o[i,t,f,aa,ab,0,0] - (
+                            r[i,t,f,aa,ab,0,0] = r[i,t,f,aa,ab,0,0] - (
                             g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
                             g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
                             g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
                             g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
 
-                            r[i,t,f,aa,ab,0,1] = o[i,t,f,aa,ab,0,1] - (
+                            r[i,t,f,aa,ab,0,1] = r[i,t,f,aa,ab,0,1] - (
                             g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
                             g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
                             g[d,rr,rc,aa,0,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
                             g[d,rr,rc,aa,0,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
 
-                            r[i,t,f,aa,ab,1,0] = o[i,t,f,aa,ab,1,0] - (
+                            r[i,t,f,aa,ab,1,0] = r[i,t,f,aa,ab,1,0] - (
                             g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,0] + \
                             g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,0] + \
                             g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,0] + \
                             g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,0])
 
-                            r[i,t,f,aa,ab,1,1] = o[i,t,f,aa,ab,1,1] - (
+                            r[i,t,f,aa,ab,1,1] = r[i,t,f,aa,ab,1,1] - (
                             g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,0]*gh[d,rr,rc,ab,0,1] + \
                             g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
                             g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
                             g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1])
 
+
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jh(np.ndarray[complex3264, ndim=8] m,
-                 np.ndarray[complex3264, ndim=6] g,
-                 np.ndarray[complex3264, ndim=8] jh,
+def cycompute_jh(complex3264 [:,:,:,:,:,:,:,:] m,
+                 complex3264 [:,:,:,:,:,:] g,
+                 complex3264 [:,:,:,:,:,:,:,:] jh,
                  int t_int,
                  int f_int):
 
@@ -110,19 +110,20 @@ def cycompute_jh(np.ndarray[complex3264, ndim=8] m,
                             jh[d,i,t,f,aa,ab,1,1] = g[d,rr,rc,aa,1,0]*m[d,i,t,f,aa,ab,0,1] + \
                                                     g[d,rr,rc,aa,1,1]*m[d,i,t,f,aa,ab,1,1]
 
+
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jhwr(np.ndarray[complex3264, ndim=8] jh,
-                  np.ndarray[complex3264, ndim=7] r,
-                  np.ndarray[np.double_t, ndim=5] w,
-                  np.ndarray[complex3264, ndim=6] jhwr,
+def cycompute_jhwr(complex3264 [:,:,:,:,:,:,:,:] jh,
+                  complex3264 [:,:,:,:,:,:,:] r,
+                  complex3264 [:,:,:,:,:,:] w,
+                  complex3264 [:,:,:,:,:,:] jhwr,
                   int t_int,
                   int f_int):
 
     """
-    This computes the jhwr term on the GN/LM method. Note that while jh is indexed by model, the 
+    This computes the jhwr term on the weigted GN/LM method. Note that while jh is indexed by model, the 
     resulting jhwr has no model index. 
     """
 
@@ -144,28 +145,28 @@ def cycompute_jhwr(np.ndarray[complex3264, ndim=8] jh,
                     for aa in xrange(n_ant):
                         for ab in xrange(n_ant):
                             jhwr[d,rr,rc,aa,0,0] = jhwr[d,rr,rc,aa,0,0] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,0] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,0]
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,0]
 
                             jhwr[d,rr,rc,aa,0,1] = jhwr[d,rr,rc,aa,0,1] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,1] + \
-                                                    w[i,t,aa,ab]*r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,1]
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,0,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,0,1]*jh[d,i,t,f,ab,aa,1,1]
 
                             jhwr[d,rr,rc,aa,1,0] = jhwr[d,rr,rc,aa,1,0] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,0] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,0]
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,0]
 
                             jhwr[d,rr,rc,aa,1,1] = jhwr[d,rr,rc,aa,1,1] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,1] + \
-                                                    w[i,t,f,aa,ab]*r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,1]
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,1,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                                                    w[i,t,f,aa,ab,0]*r[i,t,f,aa,ab,1,1]*jh[d,i,t,f,ab,aa,1,1]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jhwj(np.ndarray[complex3264, ndim=8] jh,
-                  np.ndarray[np.double_t, ndim=5] w,  
-                  np.ndarray[complex3264, ndim=6] jhwj,
+def cycompute_jhwj(complex3264[:,:,:,:,:,:,:,:] jh,
+                  complex3264[:,:,:,:,:,:] w,  
+                  complex3264[:,:,:,:,:,:]jhwj,
                   int t_int,
                   int f_int):
 
@@ -191,105 +192,129 @@ def cycompute_jhwj(np.ndarray[complex3264, ndim=8] jh,
                     for aa in xrange(n_ant):
                         for ab in xrange(n_ant):
                             jhwj[d,rr,rc,aa,0,0] = jhwj[d,rr,rc,aa,0,0] + \
-                            jh[d,i,t,f,ab,aa,0,0].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,0,0] + \
-                            jh[d,i,t,f,ab,aa,1,0].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,1,0]
+                            jh[d,i,t,f,ab,aa,0,0].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                            jh[d,i,t,f,ab,aa,1,0].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,1,0]
 
                             jhwj[d,rr,rc,aa,0,1] = jhwj[d,rr,rc,aa,0,1] + \
-                            jh[d,i,t,f,ab,aa,0,0].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,0,1] + \
-                            jh[d,i,t,f,ab,aa,1,0].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,1,1]
+                            jh[d,i,t,f,ab,aa,0,0].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                            jh[d,i,t,f,ab,aa,1,0].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,1,1]
 
                             jhwj[d,rr,rc,aa,1,0] = jhwj[d,rr,rc,aa,1,0] + \
-                            jh[d,i,t,f,ab,aa,0,1].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,0,0] + \
-                            jh[d,i,t,f,ab,aa,1,1].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,1,0]
+                            jh[d,i,t,f,ab,aa,0,1].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,0,0] + \
+                            jh[d,i,t,f,ab,aa,1,1].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,1,0]
 
                             jhwj[d,rr,rc,aa,1,1] = jhwj[d,rr,rc,aa,1,1] + \
-                            jh[d,i,t,f,ab,aa,0,1].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,0,1] + \
-                            jh[d,i,t,f,ab,aa,1,1].conjugate()*w[i,t,f,aa,ab]*jh[d,i,t,f,ab,aa,1,1]
+                            jh[d,i,t,f,ab,aa,0,1].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,0,1] + \
+                            jh[d,i,t,f,ab,aa,1,1].conjugate()*w[i,t,f,aa,ab,0]*jh[d,i,t,f,ab,aa,1,1]
+
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_jhwjinv(np.ndarray[complex3264, ndim=6] jhwj,
-                     np.ndarray[complex3264, ndim=6] jhwjinv):
+def cycompute_jhwjinv(complex3264 [:,:,:,:,:,:] jhj,
+                     complex3264 [:,:,:,:,:,:] jhjinv,
+                     np.uint16_t [:,:,:,:] flags,
+                     float eps,
+                     int flagbit):
     """
-    This inverts the approximation to the Hessian, jhwj. Note that as a useful side effect, it is 
+    This inverts the approximation to the Hessian, jhj. Note that as a useful side effect, it is 
     also suitable for inverting the gains.
-    """
 
+    Returns number of points flagged
+    """
 
     cdef int d, t, f, aa, ab = 0
     cdef int n_dir, n_tim, n_fre, n_ant
     cdef complex3264 denom = 0
+    cdef int flag_count = 0
 
-    n_dir = jhwj.shape[0]
-    n_tim = jhwj.shape[1]
-    n_fre = jhwj.shape[2]
-    n_ant = jhwj.shape[3]
+    eps = eps**2
+
+    n_dir = jhj.shape[0]
+    n_tim = jhj.shape[1]
+    n_fre = jhj.shape[2]
+    n_ant = jhj.shape[3]
 
     for d in xrange(n_dir):
         for t in xrange(n_tim):
             for f in xrange(n_fre):
                 for aa in xrange(n_ant):
+                    if flags[d,t,f,aa]:
 
-                    denom = jhwj[d,t,f,aa,0,0] * jhwj[d,t,f,aa,1,1] - \
-                            jhwj[d,t,f,aa,0,1] * jhwj[d,t,f,aa,1,0]
+                            jhjinv[d,t,f,aa,0,0] = 0
+                            jhjinv[d,t,f,aa,1,1] = 0
+                            jhjinv[d,t,f,aa,0,1] = 0
+                            jhjinv[d,t,f,aa,1,0] = 0
 
-                    if denom==0:
-                        denom = 1
+                    else:
+                        denom = jhj[d,t,f,aa,0,0] * jhj[d,t,f,aa,1,1] - \
+                                jhj[d,t,f,aa,0,1] * jhj[d,t,f,aa,1,0]
 
-                    jhwjinv[d,t,f,aa,0,0] = jhwj[d,t,f,aa,1,1]/denom
-                    jhwjinv[d,t,f,aa,1,1] = jhwj[d,t,f,aa,0,0]/denom
-                    jhwjinv[d,t,f,aa,0,1] = -1 * jhwj[d,t,f,aa,0,1]/denom
-                    jhwjinv[d,t,f,aa,1,0] = -1 * jhwj[d,t,f,aa,1,0]/denom
+                        if (denom*denom.conjugate()).real<=eps:
 
+                            jhjinv[d,t,f,aa,0,0] = 0
+                            jhjinv[d,t,f,aa,1,1] = 0
+                            jhjinv[d,t,f,aa,0,1] = 0
+                            jhjinv[d,t,f,aa,1,0] = 0
+
+                            flags[d,t,f,aa] = flagbit
+                            flag_count += 1
+
+                        else:
+
+                            jhjinv[d,t,f,aa,0,0] = jhj[d,t,f,aa,1,1]/denom
+                            jhjinv[d,t,f,aa,1,1] = jhj[d,t,f,aa,0,0]/denom
+                            jhjinv[d,t,f,aa,0,1] = -1 * jhj[d,t,f,aa,0,1]/denom
+                            jhjinv[d,t,f,aa,1,0] = -1 * jhj[d,t,f,aa,1,0]/denom
+
+    return flag_count
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_update(np.ndarray[complex3264, ndim=6] jhwr,
-                     np.ndarray[complex3264, ndim=6] jhwj,
-                     np.ndarray[complex3264, ndim=6] upd):
+def cycompute_update(complex3264 [:,:,:,:,:,:] jhr,
+                     complex3264 [:,:,:,:,:,:] jhj,
+                     complex3264 [:,:,:,:,:,:] upd):
     """
-    This computes the update by computing the product of jhwj and jhwr. These should already have been
+    This computes the update by computing the product of jhj and jhr. These should already have been
     reduced to the correct dimension so that this operation is very simple. 
     """
 
     cdef int d, t, f, aa = 0
     cdef int n_dir, n_tim, n_fre, n_ant
 
-    n_dir = jhwr.shape[0]
-    n_tim = jhwr.shape[1]
-    n_fre = jhwr.shape[2]
-    n_ant = jhwr.shape[3]
+    n_dir = jhr.shape[0]
+    n_tim = jhr.shape[1]
+    n_fre = jhr.shape[2]
+    n_ant = jhr.shape[3]
 
     for d in xrange(n_dir):
         for t in xrange(n_tim):
             for f in xrange(n_fre):
                 for aa in xrange(n_ant):
 
-                    upd[d,t,f,aa,0,0] = jhwr[d,t,f,aa,0,0]*jhwj[d,t,f,aa,0,0] + \
-                                        jhwr[d,t,f,aa,0,1]*jhwj[d,t,f,aa,1,0]
+                    upd[d,t,f,aa,0,0] = jhr[d,t,f,aa,0,0]*jhj[d,t,f,aa,0,0] + \
+                                        jhr[d,t,f,aa,0,1]*jhj[d,t,f,aa,1,0]
 
-                    upd[d,t,f,aa,0,1] = jhwr[d,t,f,aa,0,0]*jhwj[d,t,f,aa,0,1] + \
-                                        jhwr[d,t,f,aa,0,1]*jhwj[d,t,f,aa,1,1]
+                    upd[d,t,f,aa,0,1] = jhr[d,t,f,aa,0,0]*jhj[d,t,f,aa,0,1] + \
+                                        jhr[d,t,f,aa,0,1]*jhj[d,t,f,aa,1,1]
 
-                    upd[d,t,f,aa,1,0] = jhwr[d,t,f,aa,1,0]*jhwj[d,t,f,aa,0,0] + \
-                                        jhwr[d,t,f,aa,1,1]*jhwj[d,t,f,aa,1,0]
+                    upd[d,t,f,aa,1,0] = jhr[d,t,f,aa,1,0]*jhj[d,t,f,aa,0,0] + \
+                                        jhr[d,t,f,aa,1,1]*jhj[d,t,f,aa,1,0]
 
-                    upd[d,t,f,aa,1,1] = jhwr[d,t,f,aa,1,0]*jhwj[d,t,f,aa,0,1] + \
-                                        jhwr[d,t,f,aa,1,1]*jhwj[d,t,f,aa,1,1]
-                    
+                    upd[d,t,f,aa,1,1] = jhr[d,t,f,aa,1,0]*jhj[d,t,f,aa,0,1] + \
+                                        jhr[d,t,f,aa,1,1]*jhj[d,t,f,aa,1,1]
 
 @cython.cdivision(True)
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.nonecheck(False)
-def cycompute_corrected(np.ndarray[complex3264, ndim=6] o,
-                        np.ndarray[complex3264, ndim=6] g,
-                        np.ndarray[complex3264, ndim=6] gh,
-                        np.ndarray[complex3264, ndim=6] corr,
+def cycompute_corrected(complex3264 [:,:,:,:,:,:] o,
+                        complex3264 [:,:,:,:,:,:] g,
+                        complex3264 [:,:,:,:,:,:] gh,
+                        complex3264 [:,:,:,:,:,:] corr,
                         int t_int,
                         int f_int):
 
@@ -338,61 +363,3 @@ def cycompute_corrected(np.ndarray[complex3264, ndim=6] o,
                         g[d,rr,rc,aa,1,1]*o[t,f,aa,ab,1,0]*gh[d,rr,rc,ab,0,1] + \
                         g[d,rr,rc,aa,1,0]*o[t,f,aa,ab,0,1]*gh[d,rr,rc,ab,1,1] + \
                         g[d,rr,rc,aa,1,1]*o[t,f,aa,ab,1,1]*gh[d,rr,rc,ab,1,1]
-
-
-@cython.cdivision(True)
-@cython.wraparound(False)
-@cython.boundscheck(False)
-@cython.nonecheck(False)
-def cycompute_weights(np.ndarray[complex3264, ndim=7] r,
-                        np.ndarray[np.double_t, ndim=5] w,
-                        np.ndarray[np.double_t, ndim=3] v,
-                        int t_int,
-                        int f_int):
-    """
-    This computes the weights, given the latest residual visibilities and the v parameter.
-    w[i] = (v+2)/(v + 2*residual[i]^2). Next v is update using the newly compute weights.
-    """
-
-    cdef i, t, f, aa, ab, rr, rc = 0
-    cdef int n_mod, n_tim, n_fre, n_ant, m
-    cdef double d
-    cdef np.ndarray[np.double_t, ndim=1] winit
-    cdef np.double_t[:] wn
-
-    n_mod = r.shape[0]
-    n_tim = r.shape[1]
-    n_fre = r.shape[2]
-    n_ant = r.shape[3]
-
-    for i in xrange(n_mod):
-        for t in xrange(n_tim):
-            rr = t/t_int
-            for f in xrange(n_fre):
-                rc = f/f_int
-                for aa in xrange(n_ant):
-                    for ab in xrange(n_ant):
-                        w[i,t,f,aa,ab] = (v[i,rr,rc]+2)/(v[i,rr,rc] + \
-                                2*(r[i,t,f,aa,ab,0,0].conjugate()*r[i,t,f,aa,ab,0,0] + \
-                                r[i,t,f,aa,ab,0,1].conjugate()*r[i,t,f,aa,ab,0,1] + \
-                                r[i,t,f,aa,ab,1,0].conjugate()*r[i,t,f,aa,ab,1,0] + \
-                                r[i,t,f,aa,ab,1,1].conjugate()*r[i,t,f,aa,ab,1,1]))
-
-    rr = n_tim/t_int
-    rc = n_fre/f_int
-
-    for i in xrange(n_mod):
-        for t in xrange(0, n_tim, t_int):
-            for f in xrange(0, n_fre, f_int):
-                winit = np.reshape(w[t:t+t_int,f+f_int,:,:],(t_int*f_int*n_ant*n_ant))
-                wn = winit[np.where(winit!=0)]
-                m = len(wn)
-
-                vfunc = lambda a: special.digamma(0.5*(a+2)) - np.log(0.5*(a+2)) - special.digamma(0.5*a) + np.log(0.5*a) + (1./m)*np.sum(np.log(wn) - wn) + 1
-
-
-                d = fsolve(vfunc,v[i,t,f])
-                if d > 30 or d<2:
-                    v[i,t,f] = v[i,t,f]
-                else:
-                    v[i,t,f] = d
--- cubical/machines/complex_W_2x2_machine.py
+++ cubical/machines/complex_W_2x2_machine.py
@@ -1,19 +1,26 @@
 from cubical.machines.abstract_gain_machine import PerIntervalGains
 import numpy as np
 import cubical.kernels.cyfull_W_complex as cyfull
+import cPickle
+from scipy.optimize import fsolve
+from scipy import special
+import scipy.stats as st
 
 class ComplexW2x2Gains(PerIntervalGains):
     """
     This class implements the full complex 2x2 gain machine
     """
-    def __init__(self, model_arr, options):
+    def __init__(self, model_arr, label, options):
         PerIntervalGains.__init__(self, model_arr, options)
         self.gains     = np.empty(self.gain_shape, dtype=self.dtype)
         self.gains[:]  = np.eye(self.n_cor) #np.ones(self.ncor)
-        self.weights_shape = [self.n_mod, self.n_tim, self.n_fre, self.n_ant, self.n_ant]
-        self.weights = np.ones(self.weights_shape, dtype=np.float64)
-        self.v_shape = [self.n_mod, self.n_timint, self.n_freint]
-        self.v = 2*np.ones(self.v_shape, dtype=np.float64)
+        self.weights_shape = [self.n_mod, self.n_tim, self.n_fre, self.n_ant, self.n_ant, 1]
+        self.weights = np.ones(self.weights_shape, dtype=self.dtype)
+        self.v = 2.
+        self.weight_dict = {}
+        self.weight_dict["weights"] = {}
+        self.weight_dict["vvals"] = {}
+        self.label = label
 
     def compute_js(self, obser_arr, model_arr):
         """
@@ -41,7 +48,8 @@ class ComplexW2x2Gains(PerIntervalGains):
 
         # TODO: This breaks with the new compute residual code for n_dir > 1. Will need a fix.
         if n_dir > 1:
-            r = self.compute_residual(obser_arr, model_arr)
+            res_arr = np.empty_like(obser_arr)
+            r = self.compute_residual(obser_arr, model_arr, res_arr)
         else:
             r = obser_arr
 
@@ -53,9 +61,9 @@ class ComplexW2x2Gains(PerIntervalGains):
 
         jhwjinv = np.empty(jhwr_shape, dtype=obser_arr.dtype)
 
-        cyfull.cycompute_jhwjinv(jhwj, jhwjinv)
+        flag_count = cyfull.cycompute_jhwjinv(jhwj, jhwjinv, self.gflags, self.eps, self.flagbit)
 
-        return jhwr, jhwjinv
+        return jhwr, jhwjinv, flag_count
 
     def compute_update(self, model_arr, obser_arr, iters):
         """
@@ -73,12 +81,15 @@ class ComplexW2x2Gains(PerIntervalGains):
                 (((J^H)WJ)^-1)(J^H)WR
         """
 
-
-        jhwr, jhwjinv = self.compute_js(obser_arr, model_arr)
+        #print "starting iter, ", iters
+        jhwr, jhwjinv, flag_count = self.compute_js(obser_arr, model_arr)
 
         update = np.empty_like(jhwr)
 
         cyfull.cycompute_update(jhwr, jhwjinv, update)
+
+        if model_arr.shape[0]>1:
+            update = self.gains + update
 
         if iters % 2 == 0:
             self.gains = 0.5*(self.gains + update)
@@ -86,17 +97,30 @@ class ComplexW2x2Gains(PerIntervalGains):
             self.gains = update
 
         #Computing the weights
-        res_shape = [self.n_mod, self.n_tim, self.n_fre, self.n_ant, self.n_ant, self.n_cor, self.n_cor]
-        
-        residuals = np.zeros(res_shape, dtype=self.dtype) #We need to always compute the residuals
-        
-        residuals = self.compute_residual(obser_arr, model_arr, residuals)
-        
-        weights, v = cyfull.cycompute_weights(residuals, self.weights, self.v, self.t_int, self.f_int)
+        resid_arr = np.empty_like(obser_arr)
+        residuals = self.compute_residual(obser_arr, model_arr, resid_arr)
 
-        self.weights = weights
+    
+        covinv = self.compute_covinv(residuals)
 
-        self.v = v
+        weights = self.weights
+
+        v = self.v
+        
+        self.weights, self.v = self.cycompute_weights(residuals, covinv, weights, v, self.t_int, self.f_int)
+
+        #self.weights = weights
+        #self.v = v
+
+        self.weight_dict["weights"][iters] = self.weights
+        self.weight_dict["vvals"][iters] = self.v
+        self.weight_dict["t_int"] = self.t_int
+        self.weight_dict["f_int"] = self.f_int
+        f = open(str(self.label) + "_weights_dict.cp", "wb")
+        cPickle.dump(self.weight_dict, f)
+        f.close()
+
+        return flag_count
 
 
 
@@ -122,9 +146,123 @@ class ComplexW2x2Gains(PerIntervalGains):
 
         gains_h = self.gains.transpose(0,1,2,3,5,4).conj()
 
-        cyfull.cycompute_residual(model_arr, self.gains, gains_h, obser_arr, resid_arr, self.t_int, self.f_int)
+        resid_arr[:] = obser_arr
+
+        cyfull.cycompute_residual(model_arr, self.gains, gains_h, resid_arr, self.t_int, self.f_int)
 
         return resid_arr
+
+    def compute_covinv(self, residuals):
+        """
+        This functions computes the 4x4 covariance matrix of the residuals visibilities, 
+        and it approximtes it inverse
+
+        Args:
+            residuals (np.array) : Array containing the residuals.
+                              Shape is n_dir, n_tim, n_fre, n_ant, a_ant, n_cor, n_cor
+
+        Returns:
+            covinv (np.array) : Shape is ncor*n_cor x ncor*n_cor (4x4)
+            Array containing the diagonal elements of the inverse covariance matrix
+        """
+
+        N = self.n_tim*self.n_fre*self.n_ant*self.n_ant
+
+        res_reshaped = np.reshape(residuals,(N, 4))
+
+        w = np.reshape(self.weights, (N,1))
+
+        cov = res_reshaped.T.conjugate().dot(w*res_reshaped)/N
+
+        #print "here cov", cov
+
+        #print "here cov determine", np.linalg.det(cov)
+
+        covinv = np.linalg.pinv(cov)
+
+        #print "covinv ", covinv, covinv.dtype, covinv.shape
+
+        return covinv
+
+
+    def cycompute_weights(self, r, covinv, w, v, t_int, f_int):
+        """
+        This computes the weights, given the latest residual visibilities and the v parameter.
+        w[i] = (v+2)/(v + 2*residual[i]^2). Next v is update using the newly compute weights.
+        """
+
+        #cdef i, t, f, aa, ab = 0
+        #cdef int n_mod, n_tim, n_fre, n_ant, m
+        #cdef double d
+        #cdef complex3264 wnorm = 0
+        #cdef np.ndarray[float, ndim=1] winit
+        #cdef float[:] wn
+
+        n_mod = r.shape[0]
+        n_tim = r.shape[1]
+        n_fre = r.shape[2]
+        n_ant = r.shape[3]
+
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                for f in xrange(n_fre):
+                    for aa in xrange(n_ant):
+                        for ab in xrange(n_ant):
+                            rr = np.reshape(r[i,t,f,aa,ab,:,:],(4))
+                            w[i,t,f,aa,ab,0] = (v+8)/(v+ 2*rr.conj().T.dot(covinv.dot(rr)))
+        
+        #---------normalising to mean 1--------------------------#
+        wnew = np.real(np.reshape(w[:,:,:,:,:,:],(n_tim*n_fre*n_ant*n_ant)))
+        wnew_no_zero = wnew[np.where(wnew!=0)]
+        norm = np.average(wnew_no_zero)
+        w = w/norm              
+
+        
+        #-----------computing the v parameter---------------------#
+        def  _brute_solve_v(f, low, high):
+            """finds the value of v by brute for using Gauss newton method"""
+            root = None  # Initialization
+            x = np.linspace(low,high,100) #constraint the root to be between 2 and 30
+            y = f(x)
+            #print y
+
+            for i in range(len(x)-1):
+                if y[i]*y[i+1] < 0:
+                    root = x[i] - (x[i+1] - x[i])/(y[i+1] - y[i])*y[i]
+                    break  # Jump out of loop
+                elif y[i] == 0:       
+                    root = x[i]
+                    break  # Jump out of loop
+
+            if root is None:
+                dist = np.abs(y)
+                root = x[np.argmin(dist)]
+                print "Root not found, chosen value is %g"%root
+                return root
+            else:
+                print 'Found a root, x=%g' % root
+            return root
+
+        winit = np.real(np.reshape(w[:,:,:,:,:,:],(n_tim*n_fre*n_ant*n_ant)))
+        wn = winit[np.where(winit!=0)]
+        m = len(wn)
+
+        vfunc = lambda a: special.digamma(0.5*(a+8)) - np.log(0.5*(a+8)) - special.digamma(0.5*a) + np.log(0.5*a) + (1./m)*np.sum(np.log(wn) - wn) + 1
+
+        #d = fsolve(vfunc, v, xtol=1.49012e-08, maxfev=2000, factor=100)[0]
+        v = _brute_solve_v(vfunc, 2, 30)
+
+        #if d<2:
+        #    v = 2.
+        #elif d>30:
+        #    v = 30.
+        #else:
+        #   v = d
+
+        #print "v computed is %f"%v
+    
+        return w, v 
+
 
 
     def apply_inv_gains(self, obser_arr, corr_vis=None):
@@ -141,7 +279,7 @@ class ComplexW2x2Gains(PerIntervalGains):
 
         g_inv = np.empty_like(self.gains)
 
-        cyfull.cycompute_jhwjinv(self.gains, g_inv) # Function can invert G.
+        flag_count = cyfull.cycompute_jhwjinv(self.gains, g_inv, self.gflags, self.eps, self.flagbit) # Function can invert G.
 
         gh_inv = g_inv.transpose(0,1,2,3,5,4).conj()
 
@@ -150,4 +288,6 @@ class ComplexW2x2Gains(PerIntervalGains):
 
         cyfull.cycompute_corrected(obser_arr, g_inv, gh_inv, corr_vis, self.t_int, self.f_int)
 
-        return corr_vis
+        return corr_vis, flag_count
+
+    
