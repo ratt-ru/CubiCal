f73f6276503c74d9e73e8d3b08d5598b755d86c6
JSKenyon <jskenyon007@gmail.com>
2017-10-12 14:37:50 +0200

    Further work on documentation. Fixed another missing mock.

--- docs/conf.py
+++ docs/conf.py
@@ -215,5 +215,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyrap', 'pyrap.tables', 'pyrap.measures', 'SharedArray']
+MOCK_MODULES = ['pyrap', 'pyrap.tables', 'pyrap.measures', 'SharedArray', 'montblanc']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
