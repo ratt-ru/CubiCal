Fixed error in co-variance computation.
The was an error in co-variance computation. std2 was been used as a variable instead of std

cubical/machines/complex_W_2x2_machine.py
@@ -220,7 +220,7 @@ class ComplexW2x2Gains(PerIntervalGains):
             covinv = np.eye(4, dtype=self.dtype)
 
             if self.cov_type == "hybrid":
-                if np.max(std2) < 1:
+                if np.max(std) < 1:
                     covinv *= 1/np.max(std) 
                    
 