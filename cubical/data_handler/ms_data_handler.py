fixed PA calculation for circ feeds. Disabled PA rot for d147_test. Do not make PA machine if PA is not enabled.

cubical/data_handler/ms_data_handler.py
@@ -558,12 +558,15 @@ class MSDataHandler:
 
         self.enable_solve_parallactic_rotation = enable_solve_parallactic_rotation
         self.enable_apply_parallactic_rotation = enable_apply_parallactic_rotation
-        self.parallactic_machine = parallactic_machine(antnames, 
-                                                       antpos, 
-                                                       feed_basis=self._poltype, 
-                                                       enable_rotation=enable_solve_parallactic_rotation,
-                                                       enable_derotation=enable_apply_parallactic_rotation,
-                                                       field_centre=tuple(np.rad2deg(self.phadir)))
+        if enable_solve_parallactic_rotation or enable_apply_parallactic_rotation:
+            self.parallactic_machine = parallactic_machine(antnames,
+                                                           antpos,
+                                                           feed_basis=self._poltype,
+                                                           enable_rotation=enable_solve_parallactic_rotation,
+                                                           enable_derotation=enable_apply_parallactic_rotation,
+                                                           field_centre=tuple(np.rad2deg(self.phadir)))
+        else:
+            self.parallactic_machine = None
         pass
 
     def init_models(self, models, weights, mb_opts={}, use_ddes=False):

cubical/data_handler/ms_tile.py
@@ -831,7 +831,8 @@ class MSTile(object):
                                         print>> log(0), "  reading {} for model {} direction {}".format(model_source, imod,
                                                                                                         idir)
                                         model0 = self.dh.fetchslice(model_source, subset=table_subset)
-                                        model0 = self.dh.parallactic_machine.rotate(subset.utc_timestamps, model0, subset.antea, subset.anteb)
+                                        if self.dh.parallactic_machine is not None:
+                                            model0 = self.dh.parallactic_machine.rotate(subset.utc_timestamps, model0, subset.antea, subset.anteb)
                                         if self.dh.do_freq_rebin or self.dh.do_time_rebin:
                                             model = np.empty_like(obvis)
                                             rebinning.rebin_model(model, model0, flag_arr0,
@@ -1063,7 +1064,8 @@ class MSTile(object):
 
             if self.dh.output_column and data0['updated'][0]:
                 covis = subset.upsample(data['covis'])
-                covis = self.dh.parallactic_machine.derotate(subset.utc_timestamps, covis, subset.antea, subset.anteb)
+                if self.dh.parallactic_machine is not None:
+                    covis = self.dh.parallactic_machine.derotate(subset.utc_timestamps, covis, subset.antea, subset.anteb)
                 print>> log, "  writing {} column".format(self.dh.output_column)
                 self.dh.putslice(self.dh.output_column, covis, subset=table_subset)
 

cubical/machines/parallactic_machine.py
@@ -120,13 +120,14 @@ class parallactic_machine(object):
                 Calibration Errors in Interferometric Radio Polarimetry """
                 e = np.exp(1.0j * pa[:, aindex]).repeat(nchan)
                 ec = e * -1.0j
+                null = np.zeros_like(e)
                 N = pa.shape[0]
                 if N == 0: 
                     return np.zeros((0, nchan, 2, 2)) # special case: no data for this baseline
                 if conjugate_transpose:
-                    return np.array([e, 0, 0, ec]).T.reshape(N, nchan, 2, 2)
+                    return np.array([e, null, null, ec]).T.reshape(N, nchan, 2, 2)
                 else:
-                    return np.array([ec, 0, 0, e]).T.reshape(N, nchan, 2, 2)
+                    return np.array([ec, null, null, e]).T.reshape(N, nchan, 2, 2)
             if self.feed_basis == "linear":
                 return give_linear_mat(pa, nchan, aindex, conjugate_transpose)
             elif self.feed_basis == "circular":

test/d147_test.py
@@ -2,6 +2,7 @@
 
 import os, os.path, sys
 from casacore.tables import table
+import numpy as np
 from collections import OrderedDict
 
 def kw_to_args(**kw):
@@ -44,10 +45,12 @@ class SolverVerification(object):
         if retcode:
             raise RuntimeError("{}: return code {}".format(cmd, retcode))
         cd = table(self.msname).getcol("CORRECTED_DATA")
+        if not np.isfinite(cd).all():
+            raise RuntimeError("{}: NaNs/INFs detected in output data".format(cmd))
         c0 = table(self.refmsname).getcol(refcolname)
         diff = abs(cd-c0).max()
         logprint("*** max diff between CORRECTED_DATA and {} is {}".format(refcolname, diff))
-        if diff > tolerance:
+        if not diff <= tolerance:
             raise RuntimeError("{}: diff {} exceeds tolerance of {}".format(cmd, diff, tolerance))
 
 d147_test_list = [