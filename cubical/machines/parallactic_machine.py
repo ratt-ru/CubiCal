4184aeed32be82d90b8789fb8faec48b3f08f229
Benna Hugo <bennahugo@aol.com>
2019-01-18 11:11:30 +0200

    Typo

--- cubical/machines/parallactic_machine.py
+++ cubical/machines/parallactic_machine.py
@@ -110,7 +110,7 @@ class parallactic_machine(object):
                 N = pa.shape[0]
                 if N == 0: 
                     log.warn("This data chunk appears to be empty. You should probably check your data!")
-                    return np.zeros(0, nchan, 2, 2) # special case: no data????
+                    return np.zeros((0, nchan, 2, 2)) # special case: no data????
                 if conjugate_transpose:
                     return np.array([c, -s, s, c]).T.reshape(N, nchan, 2, 2)
                 else:
@@ -124,7 +124,7 @@ class parallactic_machine(object):
                 N = pa.shape[0]
                 if N == 0: 
                     log.warn("This data chunk appears to be empty. You should probably check your data!")
-                    return np.zeros(0, nchan, 2, 2) # special case: no data????
+                    return np.zeros((0, nchan, 2, 2)) # special case: no data????
                 if conjugate_transpose:
                     return np.array([e, 0, 0, ec]).T.reshape(N, nchan, 2, 2)
                 else:
