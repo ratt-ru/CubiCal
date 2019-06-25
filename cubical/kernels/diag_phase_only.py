765c71b5dce451de6c50ccacf942ac4ab6063321
Jonathan Kenyon <jskenyon007@gmail.com>
2019-06-25 12:07:12 +0200

    Initial commit of numba kernels.

---
+++ cubical/kernels/diag_phase_only.py
@@ -0,0 +1,162 @@
+# CubiCal: a radio interferometric calibration suite
+# (c) 2017 Rhodes University & Jonathan S. Kenyon
+# http://github.com/ratt-ru/CubiCal
+# This code is distributed under the terms of GPLv2, see LICENSE.md for details
+"""
+Cython kernels for the diagonal vis data phase-only gain machine. Functions require output arrays to be
+provided. Common dimensions of arrays are:
+
++----------------+------+
+| Dimension      | Size |
++================+======+
+| Direction      |   d  |
++----------------+------+
+| Model          |   m  |
++----------------+------+
+| Time           |   t  |
++----------------+------+
+| Time Intervals |   ti |
++----------------+------+
+| Frequency      |   f  |
++----------------+------+
+| Freq Intervals |   fi |
++----------------+------+
+| Antenna        |   a  |
++----------------+------+
+| Correlation    |   c  |
++----------------+------+
+
+"""
+
+import numpy as np
+from numba import jit, prange
+import generics
+import full_complex
+import diag_complex
+import diagdiag_complex
+import phase_only
+
+allocate_vis_array = full_complex.allocate_vis_array
+allocate_gain_array = full_complex.allocate_gain_array
+allocate_flag_array = full_complex.allocate_flag_array
+allocate_param_array = full_complex.allocate_param_array
+
+def compute_jhj(m, jhj, t_int=1, f_int=1):
+    """
+    Given the model array, computes the diagonal entries of J\ :sup:`H`\J. J\ :sup:`H`\J is computed
+    over intervals. This is an approximation of the Hessian. In the phase-only case, this
+    approximation does not depend on the gains, therefore it does not vary with iteration.
+
+    Args:
+        m (np.complex64 or np.complex128):
+            Typed memoryview of model array with dimensions (d, m, t, f, a, a, c, c).
+        jhj (np.complex64 or np.complex128):
+            Typed memoryview of J\ :sup:`H`\J array with dimensions (d, ti, fi, a, c, c). Must be zero-filled.
+        t_int (int):
+            Number of time slots per solution interval.
+        f_int (int):
+            Number of frequencies per solution interval.
+    """
+
+    n_dir = m.shape[0]
+    n_mod = m.shape[1]
+    n_tim = m.shape[2]
+    n_fre = m.shape[3]
+    n_ant = m.shape[4]
+
+    all_bls = np.array([[i,j] for i in xrange(n_ant) for j in xrange(n_ant) if i!=j])
+    n_bl = all_bls.shape[0]
+
+    broadcast_times = np.array([t//t_int for t in xrange(n_tim)])
+    broadcast_freqs = np.array([f//f_int for f in xrange(n_fre)])
+
+    for ibl in prange(n_bl):
+        aa, ab = all_bls[ibl,0], all_bls[ibl,1]
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                bt = broadcast_times[t]
+                for f in xrange(n_fre):
+                    bf = broadcast_freqs[f]
+                    for d in xrange(n_dir):
+
+                        m00 = m[d,i,t,f,aa,ab,0,0]
+                        m11 = m[d,i,t,f,aa,ab,1,1]
+
+                        jhj[d,bt,bf,aa,0,0] += (m00*m00.conjugate())
+                        jhj[d,bt,bf,aa,1,1] += (m11*m11.conjugate())
+
+def compute_jhr(gh, jh, r, jhr, t_int=1, f_int=1):
+    """
+    Given the conjugate gains, J\ :sup:`H` and the residual (or observed data, in special cases),
+    computes J\ :sup:`H`\R. J\ :sup:`H`\R is computed over intervals.
+
+    Args:
+        gh (np.complex64 or np.complex128):
+            Typed memoryview of gain array with dimension (d, ti, fi, a, c, c).
+        jh (np.complex64 or np.complex128):
+            Typed memoryview of J\ :sup:`H` array with dimensions (d, m, t, f, a, a, c, c).
+        r (np.complex64 or np.complex128):
+            Typed memoryview of residual array with dimensions (m, t, f, a, a, c, c).
+        jhr (np.complex64 or np.complex128):
+            Typed memoryview of J\ :sup:`H`\R array with dimensions (d, ti, fi, a, c, c).
+        t_int (int):
+            Number of time slots per solution interval.
+        f_int (int):
+            Number of frequencies per solution interval.
+    """
+
+    n_dir = jh.shape[0]
+    n_mod = jh.shape[1]
+    n_tim = jh.shape[2]
+    n_fre = jh.shape[3]
+    n_ant = jh.shape[4]
+    g_dir = gh.shape[0]
+
+    all_bls = np.array([[i,j] for i in xrange(n_ant) for j in xrange(n_ant) if i!=j])
+    n_bl = all_bls.shape[0]
+
+    broadcast_times = np.array([t//t_int for t in xrange(n_tim)])
+    broadcast_freqs = np.array([f//f_int for f in xrange(n_fre)])
+    broadcast_dirs = np.array([d%g_dir for d in xrange(n_dir)])
+
+    for ibl in prange(n_bl):
+        aa, ab = all_bls[ibl,0], all_bls[ibl,1]
+        for i in xrange(n_mod):
+            for t in xrange(n_tim):
+                bt = broadcast_times[t]
+                for f in xrange(n_fre):
+                    bf = broadcast_freqs[f]
+                    for d in xrange(n_dir):
+                        bd = broadcast_dirs[d]
+
+                        r00 = r[i,t,f,aa,ab,0,0]
+                        r11 = r[i,t,f,aa,ab,1,1]
+
+                        jhh00 = jh[d,i,t,f,ab,aa,0,0]
+                        jhh11 = jh[d,i,t,f,ab,aa,1,1]
+
+                        jhr[d,bt,bf,aa,0,0] += gh[bd,bt,bf,aa,0,0]*r00*jhh00
+                        jhr[d,bt,bf,aa,1,1] += gh[bd,bt,bf,aa,1,1]*r11*jhh11
+
+# Remaining kernel functions are reused.
+
+# J^H is computed assuming diagonal gains.
+compute_jh = diagdiag_complex.compute_jh
+
+# J^H.J inverse is computed assuming diagonal blocks.
+compute_jhjinv = generics.compute_diag_inverse
+
+# Residuals computed assuming diagonal gains.
+compute_residual = diagdiag_complex.compute_residual
+
+# Corrected visibilities computed assuming diagonal gains.
+compute_corrected = diagdiag_complex.compute_corrected
+
+# Gains applied as diagonal.
+apply_gains = diagdiag_complex.apply_gains
+
+# Update computed as in the general phase-only case.
+compute_update = phase_only.compute_update
+
+# # Gains inverted as diagonal.
+# invert_gains = generics.compute_diag_inverse
---
+++ cubical/kernels/rebinning.py
@@ -0,0 +1,137 @@
+# CubiCal: a radio interferometric calibration suite
+# (c) 2017 Rhodes University & Jonathan S. Kenyon
+# http://github.com/ratt-ru/CubiCal
+# This code is distributed under the terms of GPLv2, see LICENSE.md for details
+"""
+Cython kernels for various rebinning operations. Common dimensions of arrays are:
+
++----------------+------+
+| Dimension      | Size |
++================+======+
+| Direction      |   d  |
++----------------+------+
+| Model          |   m  |
++----------------+------+
+| Time           |   t  |
++----------------+------+
+| Time Intervals |   ti |
++----------------+------+
+| Frequency      |   f  |
++----------------+------+
+| Freq Intervals |   fi |
++----------------+------+
+| Antenna        |   a  |
++----------------+------+
+| Correlation    |   c  |
++----------------+------+
+
+"""
+
+import numpy as np
+from numba import jit, prange
+
+def rebin_index_columns(time, time0, antea, antea0, anteb, anteb0,
+             			ddid_col, ddid_col0, row_map):
+    """
+    Rebins indexing columns (time, ant1/2, ddid), using the given row map.
+    Time should be a zero-filled output column of the expected length (row_map[-1]+1)
+    anteab, anteb, ddid_col should be empty output columns of the expected length.
+    """
+
+    n_input_rows = time0.shape[0]
+    n_output_rows  = time.shape[0]
+
+    vis_count = np.zeros(n_output_rows, np.int64)
+
+    for input_row in xrange(n_input_rows):
+        output_row = abs(row_map[input_row])
+        vis_count[output_row] += 1
+        time[output_row] += time0[input_row]
+        antea[output_row] = antea0[input_row]
+        anteb[output_row] = anteb0[input_row]
+        ddid_col[output_row] = ddid_col0[input_row]
+
+    for output_row in xrange(n_output_rows):
+        if vis_count[output_row]:
+            time[output_row] /= vis_count[output_row]
+
+def rebin_vis(vis, vis0, uvw, uvw0, flag, flag0, weights, weights0, num_weights,
+              rebin_row_map, rebin_chan_map):
+    """
+    Rebin the input data.
+    """
+    n_input_rows, n_fre0, n_cor0 = vis0.shape[0], vis0.shape[1], vis0.shape[2]
+    n_row, n_fre, n_cor = vis.shape[0], vis.shape[1], n_cor0
+
+    sum_ww = np.zeros((n_row,n_fre,n_cor), np.float32)    # Sum of weights per each output visibility
+    sum_rw = np.zeros(n_row, np.float32)                  # Sum of weights per each output row
+
+    for row0 in xrange(n_input_rows):
+        row = rebin_row_map[row0]
+        conjugate = row<0
+        if conjugate:
+            row = -row
+
+        row_sum_weights = 0  # sum of weights of current _input_ row
+
+        for f0 in xrange(n_fre0):
+            f = rebin_chan_map[f0]
+            for c in xrange(n_cor0):
+		# Output flags accumulate all input flags across the bin with bitwise-OR.
+		# However, below we'll clear them if at least one unflagged visibility was present.
+                flag[row, f, c] |= flag0[row0, f0, c]  
+                if not flag0[row0, f0, c]:
+                    # accumulate weights
+                    if num_weights:
+                        for w in xrange(num_weights):
+                            weights[w, row, f, c] += weights0[w, row0, f0, c]
+                        ww = weights0[0, row0, f0, c]
+                    vis[row, f, c] += ww*vis0[row0, f0, c].conjugate() if conjugate else ww*vis0[row0, f0, c]
+                    sum_ww[row, f, c] += ww
+                    sum_rw[row] += ww
+                    row_sum_weights += ww
+        if row_sum_weights:
+            for i in xrange(3):
+                uvw[row, i] += row_sum_weights*uvw0[row0, i]
+
+    # Now normalize by counts and sums of the weights.
+    for row in xrange(n_row):
+        if sum_rw[row]:
+            for i in xrange(3):
+                uvw[row, i] /= sum_rw[row]
+            for f in xrange(n_fre):
+                for c in xrange(n_cor0):
+                    if sum_ww[row, f, c]:
+                        flag[row, f, c] = 0
+                        vis[row, f, c] /= sum_ww[row, f, c]
+
+def rebin_model(model, model0, flag0, rebin_row_map, rebin_chan_map):
+    """
+    Rebins a model column, following a rebin_row_map precomputed by cyrebin_vis
+    """
+    n_row0, n_fre0, n_cor0 = model0.shape[0], model0.shape[1], model0.shape[2]
+    n_row, n_fre, n_cor = model.shape[0], model0.shape[1], n_cor0
+
+    counts = np.zeros((n_row,n_fre,n_cor), np.int64)    # counts per each model visibility
+
+    for row0 in xrange(n_row0):
+        row = rebin_row_map[row0]
+        conjugate = row<0
+        if conjugate:
+            row = -row
+
+        for f0 in xrange(n_fre0):
+            f = rebin_chan_map[f0]
+            for c in xrange(n_cor0):
+                if not flag0[row0, f0, c]:
+                    counts[row, f, c] += 1
+                    model[row, f, c] += model0[row0, f0, c].conjugate() if conjugate else model0[row0, f0, c]
+
+    # now normalize by counts
+    for row in xrange(n_row):
+        for f in xrange(n_fre):
+            for c in xrange(n_cor):
+                if counts[row, f, c]:
+                    model[row, f, c] /= counts[row, f, c]
+
+
