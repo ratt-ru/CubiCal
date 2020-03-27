143361c5b2ae9778b8a4b5c5789407448bb881dd
Oleg Smirnov <osmirnov@gmail.com>
2020-03-27 22:48:58 +0200

    fixed 2D interpolation case

--- cubical/database/parameter.py
+++ cubical/database/parameter.py
@@ -551,8 +551,10 @@ class Parameter(object):
                                  for (ia, i0, i1), (ja, j0, j1) in zip(input_grid_segment0, input_grid_segment)]):
                     print("  slice {} preparing {}D interpolator for {}".format(slicer,
                         len(segment_grid), ",".join(["{}:{}".format(*seg[1:]) for seg in input_grid_segment])), file=log(2))
+                    # arseg: relevant segment of array slice
+                    arseg = arse.array[tuple(array_segment_slice)]
                     # arav: linear array of all values, adata: all unflagged values
-                    arav = arse.array[tuple(array_segment_slice)].ravel()
+                    arav = arseg.ravel()
                     adata = arav.data[~arav.mask] if arav.mask is not np.ma.nomask else arav.data
 
                     # this is used by the 2D and >2D cases, so make a function
@@ -595,29 +597,37 @@ class Parameter(object):
                     elif len(segment_grid) == 2:
                         # if any points inside the slice are masked, inpaint them with LinearNDInterpolator first,
                         # so that we have a complete 2D filled_array
-                        if arav.mask is not np.ma.nomask and arav.mask.any():
+                        if arseg.mask is not np.ma.nomask and arseg.mask.any():
                             ndinp, meshgrids = makeLinearNDInterpolator(segment_grid, arav, adata)
                             # list of coordinates where we're missing values
                             missing_grids = meshgrids[arav.mask, :]
                             # fill missing values using interpolator
-                            filled_array = arav.data.copy()
-                            filled_array[arav.mask] = ndinp(missing_grids.T)
-                            filled_array = filled_array.reshape(arse.array.shape)
+                            filled_array = arav.copy()
+                            filled_array[arav.mask] = ndinp(missing_grids)
+                            filled_array = filled_array.reshape(arseg.shape)
                         else:
-                            filled_array = arse.array.data
+                            filled_array = arseg.data
                         # make 2D interpolator
-                        interpolator = scipy.interpolate.interp2d(segment_grid[0], segment_grid[1],
-                                                                  filled_array, bounds_error=False)
+                        interpolator1 = scipy.interpolate.interp2d(segment_grid[0], segment_grid[1],
+                                                                  filled_array.T, bounds_error=False)
+                        interpolator = lambda *output_coord: interpolator1(*output_coord).T
+
                     # for >2 dims, use LinearNDInterpolator. Note that this does not extrapolate.
                     elif len(segment_grid) > 2:
-                        interpolator, _ = makeLinearNDInterpolator(segment_grid, arav, adata)
+                        interpolator1, _ = makeLinearNDInterpolator(segment_grid, arav, adata)
+                        interpolator = lambda *output_coord: \
+                            interpolator1(np.array([x.ravel() for x in np.meshgrid(*output_coord, indexing='ij')]).T).\
+                                reshape(interp_shape)
+
                     # cache the interpolator
                     self._interpolators[slicer] = interpolator, input_grid_segment
 
-                # make a meshgrid of output coordinates, and massage into correct shape for interpolator
-                coords = np.array([x.ravel() for x in np.meshgrid(*output_coord, indexing='ij')])
-                # call interpolator. Reshape into output slice shape
-                result = interpolator(coords.T).reshape(interp_shape)
+                result = interpolator(*output_coord)
+
+                # # make a meshgrid of output coordinates, and massage into correct shape for interpolator
+                # coords = np.array([x.ravel() for x in np.meshgrid(*output_coord, indexing='ij')])
+                # # call interpolator. Reshape into output slice shape
+                # result = interpolator(coords.T).reshape(interp_shape)
                 print("  interpolated onto {} grid".format("x".join(map(str, interp_shape))), file=log(2))
                 output_array[out_slicer] = result[tuple(interp_broadcast)]
         # return array, throwing out unneeded axes
